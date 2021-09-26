f5fc739d529319d670080998c3a3d1318a84d524
Artem <volkovartemka07@gmail.com>
2021-09-26 19:29:28 +0300

    Исправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
+const maxString = 100;
+const minString = 0;
+const integer = 1;
+
 const getCoordinates = function (min, max) {
-  const array = Math.random() * (max - min);
-  return array;
+  if (max > min) {
+    const array = Math.floor(Math.random() * (max - min));
+    return array;
+  } else {
+    return false;
+  }
 };
-getCoordinates(0, 100);
-const getRandom = function (min, max) {
-  const array = Math.floor(Math.random() * (max - min));
-  return array;
+getCoordinates(minString, maxString);
+
+const getRandom = function (min, max, signs) {
+  if (min >= min) {
+    const array = (Math.random() * (max - min));
+    return array.toFixed(signs);
+  } else {
+    return false;
+  }
 };
-getRandom(0, 100);
+getRandom(minString, maxString, integer);
 
 
