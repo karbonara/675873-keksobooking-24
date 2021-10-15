f9b82190b3a8ad3903587bacdcda73b448580d68
Karbonara <37299335+karbonara@users.noreply.github.com>
2021-10-16 02:22:18 +0300

    Заменил название переменных

--- js/form.js
+++ js/form.js
@@ -1,19 +1,19 @@
 const inactiveStateForm = () => {
-  const form = document.querySelector('.ad-form')
+  const form = document.querySelector('.ad-form');
   form.classList.add('ad-form--disabled');
 
   const formElement = form.querySelectorAll('.ad-form__element');
   for (let i = 0; i < formElement.length; i++) {
-    const formElements = formElement[i]
+    const formElements = formElement[i];
     formElements.classList.add('ad-form--disabled');
   }
 
   const mapFilters = document.querySelector('.map__filters');
   mapFilters.classList.add('ad-form--disabled');
 
-  const mapFilterElement = document.querySelectorAll('.map__filter')
+  const mapFilterElement = document.querySelectorAll('.map__filter');
   for (let i = 0; i < mapFilterElement.length; i++) {
-    const mapFilterElements = mapFilterElement[i]
+    const mapFilterElements = mapFilterElement[i];
     mapFilterElements.classList.add('disabled');
   }
 };
